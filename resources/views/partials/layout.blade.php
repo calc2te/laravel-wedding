b87cf67d65750d8adfc03270f251ba020bf8542c
Seobs <subi88@naver.com>
2020-03-28 15:12:57 +0900

    Add driving animation

--- resources/views/partials/layout.blade.php
+++ resources/views/partials/layout.blade.php
@@ -169,6 +169,18 @@
         });
     }
 
+    function driving_animation(emoji) {
+        dataLayer.push({
+            'item': emoji,
+            'event': 'clickEasterEgg'
+        });
+
+        document.getElementById(emoji).className ='driving';
+        setTimeout(function() {
+            document.getElementById(emoji).className ='';
+        }, 5000);
+    }
+
     // 안녕하십니까 제제입니다 만나서 방가워요 저는 제제입니다 호호하하 어색해요 사진 그만 찌거요 제제는 엄청 어색해유 그만 어여 찍으란말야
     // 하이하이호럽하이하이호럽 섭섭섭섭 비엔나커피를 마시며 사진을 찍는거는 참 어렵네요
 </script>
